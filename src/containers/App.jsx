cc35976d94fca9ed09dce1628cd79e27c8e5a07b
F1r3byte <f1r3byte@yandex.com>
2018-08-31 20:18:32 +0200

    connected to chatroom

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import Chatkit from "@pusher/chatkit";
 import RoomList from "../components/RoomList";
 import MessageList from "../components/MessageList";
 import SendMessageForm from "../components/SendMessageForm";
 import NewRoomForm from "../components/NewRoomForm";
 import "./App.css";
+import {tokenUrl, instanceLocator} from "./config";
 
 class App extends Component {
+
+	componentDidMount () {
+		const chatManager = new Chatkit.ChatManager({
+			instanceLocator,
+			userId: "johnDoe",
+			tokenProvider: new Chatkit.TokenProvider({url: tokenUrl}),
+		});
+		chatManager.connect()
+		.then(currentUser => {
+			currentUser.subscribeToRoom({
+				roomId: 15134393,
+				hooks: {
+					onNewMessage: message => {console.log('Message Text: ', message.text)}
+				}
+			})
+		})
+	}
+
 	render() {
 		return (
 			<div className="App">
