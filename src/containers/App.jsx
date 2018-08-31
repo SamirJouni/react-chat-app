36afab55babafbdcb942e077f06ff492a9b53082
F1r3byte <f1r3byte@yandex.com>
2018-08-31 19:22:25 +0200

    moved to jsx format and added components

---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import RoomList from "../components/RoomList";
+import MessageList from "../components/MessageList";
+import SendMessageForm from "../components/SendMessageForm";
+import NewRoomForm from "../components/NewRoomForm";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
