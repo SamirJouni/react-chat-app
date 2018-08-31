0abba7ff522e0c63d348dc0e2adb9618f8e868e5
F1r3byte <f1r3byte@yandex.com>
2018-08-31 19:46:42 +0200

    added sample messages from dummy data

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
+const DUMMY_DATA = [
+	{
+		senderId: "johnDoe",
+		text: "hey there, I'm new"
+	},
+	{
+		senderId: "janeDoe",
+		text: "welcome to this chatroom"
+	},
+	{
+		senderId: "johnDoe",
+		text: "oh, that's a warm welcome ! Thanks."
+	}
+];
+
 class MessageList extends Component {
 	render() {
 		return (
 			<div className="message-list">
-				<div className="help-text">Message List</div>
+				{DUMMY_DATA.map((message, i) => (
+					<div className="message" key={i}>
+						<div className="message-username">{message.senderId}</div>
+						<div className="message-text">{message.text}</div>
+					</div>
+				))}
 			</div>
 		);
 	}
