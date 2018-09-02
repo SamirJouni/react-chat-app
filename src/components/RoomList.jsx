54d5de1551b390ab43b8cfcb8bd2c3ad13c4aba5
F1r3byte <f1r3byte@yandex.com>
2018-09-02 13:34:47 +0200

    fixed rooms jumping around

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class RoomList extends Component {
 	render() {
+		const sortedRooms = [...this.props.rooms].sort((a,b) => a.id - b.id);
 		return (
 			<div className="room-list">
 				<ul>
 					<h3>Rooms:</h3>
-					{this.props.rooms.map(room => (
+					{sortedRooms.map(room => (
 						<li key={room.id} className="room">
 							<a onClick={() => this.props.subscribeToRoom(room.id)}># {room.name}</a>
 						</li>
